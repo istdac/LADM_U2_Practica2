665baa560987f317dc00a6a18117f8e3c276f361
DiegoCadena <diego.cadena@borngroup.com>
2022-11-10 16:00:39 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u2_practica2_propuesta/Juego2.kt
@@ -0,0 +1,11 @@
+package mx.edu.ittepic.ladm_u2_practica2_propuesta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Juego2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego2)
+    }
+}
---
+++ app/src/main/java/mx/edu/ittepic/ladm_u2_practica2_propuesta/MainActivity.kt
@@ -0,0 +1,32 @@
+package mx.edu.ittepic.ladm_u2_practica2_propuesta
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.edu.ittepic.ladm_u2_practica2_propuesta.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding :ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnJ1.setOnClickListener {
+            var j1 = Intent(this,Juego1::class.java)
+            startActivity(j1)
+        }
+        binding.btnJ1.setOnClickListener {
+            var j2 = Intent(this,Juego2::class.java)
+            startActivity(j2)
+        }
+        binding.btnJ1.setOnClickListener {
+            var j3 = Intent(this,Juego3::class.java)
+            startActivity(j3)
+        }
+        binding.btnJ1.setOnClickListener {
+            var j4 = Intent(this,Juego4::class.java)
+            startActivity(j4)
+        }
+
+    }//onCreate
+}
